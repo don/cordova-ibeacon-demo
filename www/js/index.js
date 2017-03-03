3a94ab0b234fe607913fedba50e658e325569e27
Don Coleman <dcoleman@chariotsolutions.com>
2017-03-02 23:18:35 -0500

    work with neopixel

--- www/js/index.js
+++ www/js/index.js
@@ -1,4 +1,6 @@
-/* global cordova, regionDiv, rangeDiv */
+/* global cordova, regionDiv, rangeDiv, ble, Uint8Array */
+var SERVICE_UUID = 'ccc0';
+var COLOR_CHARACTERISTIC_UUID = 'ccc1';
 
 var app = {
   initialize: function () {
@@ -22,6 +24,7 @@ var app = {
       cordova.plugins.locationManager.startRangingBeaconsInRegion(beaconRegion)
         .fail(function (e) { console.error(e); })
         .done();
+      app.connectToLamp();
     };
 
     delegate.didExitRegion = function (/*pluginResult*/) {
@@ -29,11 +32,13 @@ var app = {
       cordova.plugins.locationManager.stopRangingBeaconsInRegion(beaconRegion)
         .fail(function (e) { console.error(e); })
         .done();
+      app.disconnect();
     };
 
     delegate.didRangeBeaconsInRegion = function (pluginResult) {
       if (pluginResult.beacons.length > 0) {
         var proximity = pluginResult.beacons[0].proximity;
+        app.updateColor(proximity);
         rangeDiv.innerText = proximity.replace('Proximity', 'Proximity: ');
       } else {
         rangeDiv.innerText = '';
@@ -47,6 +52,44 @@ var app = {
       .fail(function (e) { console.error(e); })
       .done();
 
+  },
+  connectToLamp: function () {
+    ble.startScan([SERVICE_UUID], function(device){
+      console.log('Discovered', device.id);
+      // stop scan and connect to first device discovered
+      ble.stopScan(function(){
+        console.log('Connecting to', device.id);
+        ble.connect(device.id, function(peripheral) {
+          console.log('Connected to', peripheral.id);
+          app.peripheral = peripheral;
+        });
+      });
+    });
+  },
+  disconnect: function() {
+    if (app.peripheral) {
+      app.peripheral.disconnect();
+    }
+  },
+  updateColor: function (proximity) {
+    var color;
+    switch (proximity) {
+    case 'ProximityImmediate':
+      color = new Uint8Array([0xff,0x00, 0x00]); // Red
+      break;
+    case 'ProximityNear':
+      color = new Uint8Array([0x00, 0x00, 0xff]); // Blue
+      break;
+    case 'ProximityFar':
+      color = new Uint8Array([0x00, 0xff, 0x00]); // Green
+      break;
+    case 'ProximityUnknown':
+      color = new Uint8Array([0x00, 0x00, 0x00]); // Black/Off
+      break;
+    default:
+      console.log(proximity);
+    }
+    ble.write(app.peripheral.id, SERVICE_UUID, COLOR_CHARACTERISTIC_UUID, color.buffer);
   }
 };
 
